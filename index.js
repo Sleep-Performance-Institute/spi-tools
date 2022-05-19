5a80fa0ae40fde0456042b960259863ae695e626
Philip Grey <philip@philipgrey.com>
2022-05-19 14:54:42 -0500

    Coaching.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ exports.createClient = require('./lib/create-client')
 
 exports.getOnboardingCarousel = require('./lib/getters/get-onboarding-carousel')
 exports.getDashboard = require('./lib/getters/get-dashboard')
+exports.getCoaching = require('./lib/getters/get-coaching')
 exports.getAssessment = require('./lib/getters/get-assessment')
 exports.getDisorder = require('./lib/getters/get-disorder')
 exports.getLibraryCarousels = require('./lib/getters/get-library-carousels')
